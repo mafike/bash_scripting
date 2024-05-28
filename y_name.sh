edbdd1f956290510f33966257b50c1e97c821414
mafike <04_bronze.squirm@icloud.com>
2024-05-28 03:14:12 +0000

    ex1

---
+++ y_name.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Bash if statement 
+read -p "What is your name? " name
+if [[ -z ${name} ]]
+then
+	 echo "Please enter your name!"
+else
+         echo " Hi there ${name} "	
+fi
+
+
+
+
+
